8f97b69cd2d5d2abaf661d30985e5624c6822ef6
Arc_U <tammy.hills@geoexcenter.org>
2025-03-09 18:45:38 -0600

    Test_7b

--- assignment7/BasicMap.py
+++ assignment7/BasicMap.py
@@ -55,11 +55,11 @@ if __name__ == "__main__":
     my_map = BasicMap("-105.2705", "40.015", "0.5", "0.25")
     my_map.describe()
 
-
-    print('Calculating bounds...')
-    my_map.get_bounds()
-
-    print('Error: in get_bounds - input values must be numbers!')
+    try:
+        print('Calculating bounds...')
+        my_map.get_bounds()
+    except TypeError:
+        print('Error: in get_bounds - input values must be numbers!')
 
 
 
